3008c1709442293a01a0977924fa959708209ab9
zsecducna <76715938+zsecducna@users.noreply.github.com>
2021-01-09 00:19:40 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,3 @@
-riot.mapboxAccessToken = 'pk.eyJ1IjoiYW56YWM3IiwiYSI6ImNraTR6NnB5ODIybDYycW8zeXQyNGM5YTIifQ.dpnMADb3jYp5f-fSehq0DA';
-        riot.transactionFee = '5%';
-        riot.currency = 'USD';
-        
-            riot.auth0user = {'picture': 'https://s.gravatar.com/avatar/679696b112e8bdfa55f143f2ed876644?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Fzs.png', 'customPhoto': 'https://s3-ap-southeast-2.amazonaws.com/stage2-app-media.earth2.io/thumbnails/cd68428a-dc48-4333-b60c-d44435690634_avatar_e2dd884b-2fa7-4759-9efc-a5d0fed1c2bd.tml', 'first_name': 'zsec.ducna@gmail.com', 'username': 'zzzz', 'phoneNumber': 'undefined', 'email': 'zsec.ducna@gmail.com', 'referralCodeId': '0K4NX02ZUR', 'referrerCodeId': '', 'id': 'cd68428a-dc48-4333-b60c-d44435690634', 'eth_address': 'None', 'preferredView': 'streets', 'notificationCount': 7, 'countryCode': 'vn', 'countryFlag': 'https://stage2-app-static.earth2.io/assets/flags/vn.png', 'balance': 1009.58, 'networth': 3500.03, 'totalTiles': 31, 'spent': 2.8100001};
-        
-    
-
-    static_url = "https://stage2-app-static.earth2.io/";
-    paypal_env = "sandbox";
-
-    riot.mount('app', {});
-
-    route.base('#');
-    route.start(true);
-    alert("XXXXXX");
+document.getElementById("buyBtn").click();
+document.getElementById("payBtn").click();
+document.getElementById("pay_confirm_yes").click();
